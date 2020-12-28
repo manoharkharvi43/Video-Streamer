3c8e056f183df1a9db582824c37628059525410d
manoharkharvi43 <manoharkharvi43@gmail.com>
2020-12-28 12:49:18 +0530

    first commit

---
+++ Components/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function VideoPlayer() {
+	return (
+		<View>
+			<Text>Hello React</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
